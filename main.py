50a1ac97a759e8ff52b6129abfc448268a05b3c9
SEKANG OH <62413137+ohsekang@users.noreply.github.com>
2023-09-14 11:53:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,13 +8,13 @@ from SurroundBirdEyeView import BevGenerator
 
 def runInCalib_1():
     print("Intrinsic Calibration ......")
-    calibrator = InCalibrator('fisheye')                # 初始化内参标定器
+    calibrator = InCalibrator('fisheye')                # 내부 매개변수 교정기 초기화
     PATH = './IntrinsicCalibration/data/'
     images = os.listdir(PATH)
     for img in images:
         print(PATH + img)
         raw_frame = cv2.imread(PATH + img)
-        result = calibrator(raw_frame)                  # 每次读入一张原始图片 更新标定结果
+        result = calibrator(raw_frame)                  # 원본 이미지를 읽을 때마다 교정 결과가 업데이트됩니다.
 
     print("Camera Matrix is : {}".format(result.camera_mat.tolist()))
     print("Distortion Coefficient is : {}".format(result.dist_coeff.tolist()))
@@ -22,43 +22,43 @@ def runInCalib_1():
 
     raw_frame = cv2.imread('./IntrinsicCalibration/data/img_raw0.jpg')
     cv2.imshow("Raw Image", raw_frame)
-    undist_img = calibrator.undistort(raw_frame)        # 使用undistort方法得到去畸变图像
+    undist_img = calibrator.undistort(raw_frame)        # 왜곡되지 않은 방법을 사용하여 왜곡된 이미지를 얻습니다.
     cv2.imshow("Undistorted Image", undist_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 def runInCalib_2():
     print("Intrinsic Calibration ......")
-    args = InCalibrator.get_args()                      # 获取内参标定args参数
-    args.INPUT_PATH = './IntrinsicCalibration/data/'    # 修改为新的参数
-    calibrator = InCalibrator('fisheye')                # 初始化内参标定器
-    calib = CalibMode(calibrator, 'image', 'auto')      # 选择标定模式
-    result = calib()                                    # 开始标定
+    args = InCalibrator.get_args()                      # 내부 매개변수 교정 인수 매개변수 가져오기
+    args.INPUT_PATH = './IntrinsicCalibration/data/'    # 새 매개변수로 수정
+    calibrator = InCalibrator('fisheye')                # 내부 매개변수 교정기 초기화
+    calib = CalibMode(calibrator, 'image', 'auto')      # 교정 모드 선택
+    result = calib()                                    # 교정 시작
 
     print("Camera Matrix is : {}".format(result.camera_mat.tolist()))
     print("Distortion Coefficient is : {}".format(result.dist_coeff.tolist()))
     print("Reprojection Error is : {}".format(np.mean(result.reproj_err)))
 
     raw_frame = cv2.imread('./IntrinsicCalibration/data/img_raw0.jpg')
-    # calibrator.draw_corners(raw_frame)                  # 画出角点
+    # calibrator.draw_corners(raw_frame)                  # 모서리 점 그리기
     cv2.imshow("Raw Image", raw_frame)
-    undist_img = calibrator.undistort(raw_frame)        # 使用undistort方法得到去畸变图像
+    undist_img = calibrator.undistort(raw_frame)        # 왜곡되지 않은 방법을 사용하여 왜곡된 이미지를 얻습니다.
     cv2.imshow("Undistort Image", undist_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
 
 def runExCalib():
     print("Extrinsic Calibration ......")
-    exCalib = ExCalibrator()                            # 初始化外参标定器
+    exCalib = ExCalibrator()                            # 외부 매개변수 교정기 초기화
 
     src_raw = cv2.imread('./ExtrinsicCalibration/data/img_src_back.jpg')
     dst_raw = cv2.imread('./ExtrinsicCalibration/data/img_dst_back.jpg')
 
-    homography = exCalib(src_raw, dst_raw)              # 输入对应的两张去畸变图像 得到单应性矩阵
+    homography = exCalib(src_raw, dst_raw)              # 호모그래피 행렬을 얻기 위해 해당하는 두 개의 왜곡된 이미지를 입력합니다.
     print("Homography Matrix is:")
     print(homography.tolist())
 
-    src_warp = exCalib.warp()                           # 使用warp方法得到原始图像的变换结果
+    src_warp = exCalib.warp()                           # Warp 메소드를 사용하여 원본 이미지의 변환 결과 얻기
 
     cv2.namedWindow("Source View", flags=cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
     cv2.imshow("Source View", src_raw)
@@ -76,12 +76,12 @@ def runBEV():
     left = cv2.imread('./SurroundBirdEyeView/data/left/left.jpg')
     right = cv2.imread('./SurroundBirdEyeView/data/right/right.jpg')
 
-    args = BevGenerator.get_args()                      # 获取环视鸟瞰args参数
+    args = BevGenerator.get_args()                      # 조감도 인수 매개변수 가져오기
     args.CAR_WIDTH = 200
-    args.CAR_HEIGHT = 350                               # 修改为新的参数
+    args.CAR_HEIGHT = 350                               # 새 매개변수로 수정
 
-    bev = BevGenerator(blend=True, balance=True)        # 初始化环视鸟瞰图生成器
-    surround = bev(front, back, left, right)            # 输入前后左右四张原始相机图像 得到拼接后的鸟瞰图
+    bev = BevGenerator(blend=True, balance=True)        # 둘러보기 조감도 생성기 초기화
+    surround = bev(front, back, left, right)            # 전면, 후면, 왼쪽, 오른쪽의 4개의 원본 카메라 이미지를 입력하여 접합된 조감도를 얻습니다.
 
     cv2.namedWindow('surround', flags=cv2.WINDOW_NORMAL | cv2.WINDOW_KEEPRATIO)
     cv2.imshow('surround', surround)
